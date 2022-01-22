aa4250adbd1c7e9fa664a4806f34dabf0f1dd7be
ValiaStohnii <valia19961018@gmail.com>
2022-01-22 19:10:45 +0200

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,75 @@
-import React, { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { useState } from 'react';
 import './App.css';
 import Searchbar from './Components/Searchbar/Searchbar';
 import ImageGallery from './Components/ImageGallery/ImageGallery';
 import Modal from './Components/Modal/Modal';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-class App extends Component {
-  state = {
-    imageName: '',
-    showModal: false,
-    selectIMG: '',
+export default function App() {
+  const [imageName, setImageName] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectIMG, setSelectIMG] = useState('');
+
+  const toggleShowModal = () => {
+    setShowModal(state => !state.showModal);
   };
 
-  formSubmit = imageName => {
-    this.setState({ imageName });
+  const toggleModal = ({ largeIMG }) => {
+    setShowModal(state => !state.showModal);
+    setSelectIMG(largeIMG);
   };
 
-  toggleShowModal = () => {
-    this.setState(state => ({
-      showModal: !state.showModal,
-    }));
-  };
-
-  toggleModal = ({ largeIMG }) => {
-    this.setState(state => ({
-      showModal: !state.showModal,
-      selectIMG: largeIMG,
-    }));
-  };
-
-  render() {
-    const { showModal, imageName, selectIMG } = this.state;
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.formSubmit} />
-        <ToastContainer />
-        <ImageGallery imageName={imageName} onImageClick={this.toggleModal} />
-        {showModal && (
-          <Modal onClose={this.toggleShowModal}>
-            <img src={selectIMG} alt={imageName} />
-          </Modal>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Searchbar onSubmit={setImageName} />
+      <ImageGallery imageName={imageName} onImageClick={toggleModal} />
+      {showModal && (
+        <Modal onClose={toggleShowModal}>
+          <img src={selectIMG} alt={imageName} />
+        </Modal>
+      )}
+    </div>
+  );
 }
+// class App extends Component {
+//   state = {
+//     imageName: '',
+//     showModal: false,
+//     selectIMG: '',
+//   };
 
-export default App;
+//   formSubmit = imageName => {
+//     this.setState({ imageName });
+//   };
+
+//   toggleShowModal = () => {
+//     this.setState(state => ({
+//       showModal: !state.showModal,
+//     }));
+//   };
+
+//   toggleModal = ({ largeIMG }) => {
+//     this.setState(state => ({
+//       showModal: !state.showModal,
+//       selectIMG: largeIMG,
+//     }));
+//   };
+
+//   render() {
+//     const { showModal, imageName, selectIMG } = this.state;
+//     return (
+//       <div className="App">
+//         <Searchbar onSubmit={this.formSubmit} />
+//         <ToastContainer />
+//         <ImageGallery imageName={imageName} onImageClick={this.toggleModal} />
+//         {showModal && (
+//           <Modal onClose={this.toggleShowModal}>
+//             <img src={selectIMG} alt={imageName} />
+//           </Modal>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,48 +1,86 @@
-import React, { Component } from 'react';
-import { toast } from 'react-toastify';
+import { useState } from 'react';
 import './Searchbar.css';
 
-class Searchbar extends Component {
-  state = {
-    imageName: '',
-  };
+export default function Searchbar({ onSubmit }) {
+  const [imageName, setImageName] = useState('');
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.imageName.trim() === '') {
-      toast('Введіть назву картинки');
+    if (imageName.trim() === '') {
+      alert('Введіть назву картинки');
       return;
     }
-    this.props.onSubmit(this.state.imageName);
-    this.setState({ imageName: '' });
+    onSubmit(imageName);
+    setImageName('');
   };
 
-  handleNameChange = e => {
-    this.setState({ imageName: e.currentTarget.value.toLowerCase() });
+  const handleNameChange = e => {
+    setImageName(e.currentTarget.value.toLowerCase());
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form onSubmit={this.handleSubmit} className="SearchForm">
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
+  return (
+    <header className="Searchbar">
+      <form onSubmit={handleSubmit} className="SearchForm">
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
 
-          <input
-            onChange={this.handleNameChange}
-            value={this.state.imageName}
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          onChange={handleNameChange}
+          value={imageName}
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
 
-export default Searchbar;
+// class Searchbar extends Component {
+//   state = {
+//     imageName: '',
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     if (this.state.imageName.trim() === '') {
+//       toast('Введіть назву картинки');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.imageName);
+//     this.setState({ imageName: '' });
+//   };
+
+//   handleNameChange = e => {
+//     this.setState({ imageName: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   render() {
+//     return (
+//       <header className="Searchbar">
+//         <form onSubmit={this.handleSubmit} className="SearchForm">
+//           <button type="submit" className="SearchForm-button">
+//             <span className="SearchForm-button-label">Search</span>
+//           </button>
+
+//           <input
+//             onChange={this.handleNameChange}
+//             value={this.state.imageName}
+//             className="SearchForm-input"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Searchbar;
